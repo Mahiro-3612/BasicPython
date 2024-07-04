64be104d9e2be92ba7e76626f8e383e95ba37fce
Mahiro-3612 <m.h.s.k.australia@gmail.com>
2024-07-04 21:13:16 +0900

    元々関数で実装していたものが多かったです

--- prime_number.py
+++ prime_number.py
@@ -1,22 +1,35 @@
-def prime_number_checker(input):
+def prime_number_checker(n):
 
-    if input == 2:
-        return "素数です"
+    result_flag = True
+    try:
+        if not isinstance(n, int) or n < 1:
+            raise ValueError("自然数ではありません")
 
-    else:
-        possible_factor = 2
+        elif n == 1:
+            return False
 
-        while possible_factor < input // 2 + 1:
-            if input % possible_factor == 0:
-                return "素数ではありません"
-                break
-            else:
-                possible_factor += 1
+        elif n == 2:
+            return True
 
-    return "素数です"
+        else:
+            result_flag = True
+            possible_factor = 2
+
+            while possible_factor < n // 2 + 1:
+                if n % possible_factor == 0:
+                    result_flag = False
+                    break
+                else:
+                    possible_factor += 1
+
+            return result_flag
+    except ValueError as e:
+        return None, f"エラー: {e}"
 
 
 # (1)
 print(f"61は{prime_number_checker(61)}")
 # (2)
 print(f"10は{prime_number_checker(10)}")
+# エクストラ
+print(f"1.1は{prime_number_checker(1.1)}")
