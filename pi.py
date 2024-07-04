2918aadf9418c9fafb70de917e30fb128117aeab
Mahiro-3612 <m.h.s.k.australia@gmail.com>
2024-07-04 16:19:07 +0900

    こんなに円周率覚えてないです。

--- pi.py
+++ pi.py
@@ -3,5 +3,20 @@ text = """
     quantum mechanics. All of thy geometry, Herr Planck, is fairly hard.
 """
 
-# TODO
 
+def letter_count_sequence(input):
+
+    input = input.replace(",", "").replace(".", "").split()
+
+    letter_counts = list(map(len, input))
+
+    output = ""
+
+    for letter_count in letter_counts:
+        output += str(letter_count)
+
+    return output
+
+
+# 問2
+print(f"円周率は{letter_count_sequence(text)}")
