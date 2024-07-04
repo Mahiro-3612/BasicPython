aa7c4cd628e260ee891a0846d3baaad6b93a77c5
Mahiro-3612 <m.h.s.k.australia@gmail.com>
2024-07-04 15:51:30 +0900

    関数で実装してみました。

--- quadratic_formula.py
+++ quadratic_formula.py
@@ -1,9 +1,26 @@
-a = 1
-b = -6
-c = 9
+import cmath
 
-# TODO
-x1 = ...
-x2 = ...
 
-print(x1, x2)
+def quadratic_formula(a, b, c):
+
+    if a == 0:
+        raise ValueError("二次式ではありません")
+
+    elif b**2 - 4 * a * c == 0:
+        x = -b / (2*a)
+        return x
+    
+    else:
+        x1 = (-b - cmath.sqrt(b**2 - 4 * a * c)) / (2*a)
+        x2 = (-b + cmath.sqrt(b**2 - 4 * a * c)) / (2*a)
+        return x1, x2
+
+
+# (1)
+print(f"(1)の答えは、{quadratic_formula(1, -6, 9)}")
+# (2)
+print(f"(2)の答えは、{quadratic_formula(1, 2, -8)}")
+# (3)
+print(f"(3)の答えは、{quadratic_formula(8, -6, 35)}")
+# (4)
+print(f"(4)の答えは、{quadratic_formula(1, 0, 1)}")
