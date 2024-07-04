064aa51391c9ccbe6877d31bc6091c78f87ef369
Mahiro-3612 <m.h.s.k.australia@gmail.com>
2024-07-04 17:58:45 +0900

    制御構文のチェックよろしくお願いします。

--- euclid.py
+++ euclid.py
@@ -1,4 +1,22 @@
-a = input("a の値を入力: ")
-b = input("b の値を入力: ")
+def euclid(a, b):
 
-# TODO
+    if a < b:
+        a, b = b, a
+
+    if a % b == 0:
+        return b
+    else:
+        while a % b != 0:
+            remainder = a % b
+            a = b
+            b = remainder
+
+        return b
+
+
+# (1)
+print(f"10と20の最大公約数は{euclid(10, 20)}")
+# (2)
+print(f"14と91の最大公約数は{euclid(14, 91)}")
+# (3)
+print(f"91と14の最大公約数は{euclid(91, 14)}")
