bd2d55be082127234c328e9d10467872874c3be4
Mahiro-3612 <m.h.s.k.australia@gmail.com>
2024-07-06 14:03:21 +0900

    修正版です

--- trapezoidal_integral.py
+++ trapezoidal_integral.py
@@ -1,17 +1,31 @@
 import math
 
 
-def trapezoidal_integral(func, a, b, N):
+def trapezoidal_integral(f, a=0, b=1, n=100):
 
-    h = (b - a) / N
+    h = (b - a) / n
 
     S = 0
 
-    for k in range(1, N + 1):
-        S += (h / 2) * (func(a + (k - 1) * h) + func(a + k * h))
+    for k in range(1, n + 1):
+        S += (h / 2) * (f(a + (k - 1) * h) + f(a + k * h))
 
     return S
 
 
-# (問2)
-print(f"求める面積は{trapezoidal_integral(math.sin, 0, math.pi / 2, 100)}")
+def func2(x):
+    return 4 / (1 + x ** 2)
+
+
+def func3(x):
+    return math.sqrt(math.pi) * math.exp(-x ** 2)
+
+
+# 関数問2(1)
+print(f"求める面積は{trapezoidal_integral(math.sin, 0, math.pi / 2, 50)}")
+
+# 関数問2(2)
+print(f"求める面積は{trapezoidal_integral(func2, 0, 1, 100)}")
+
+# 関数問2(3)
+print(f"求める面積は{trapezoidal_integral(func3, -100, 100, 1000)}")
