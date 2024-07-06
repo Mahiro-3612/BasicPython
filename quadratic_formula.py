3b9cf29c457e06070e6d7835c9089679f88d26e3
Mahiro-3612 <m.h.s.k.australia@gmail.com>
2024-07-06 13:03:01 +0900

    (3)で代入する値を修正しました

--- quadratic_formula.py
+++ quadratic_formula.py
@@ -9,7 +9,7 @@ def quadratic_formula(a, b, c):
     elif b**2 - 4 * a * c == 0:
         x = -b / (2*a)
         return x
-    
+
     else:
         x1 = (-b - cmath.sqrt(b**2 - 4 * a * c)) / (2*a)
         x2 = (-b + cmath.sqrt(b**2 - 4 * a * c)) / (2*a)
@@ -21,6 +21,6 @@ print(f"(1)の答えは、{quadratic_formula(1, -6, 9)}")
 # (2)
 print(f"(2)の答えは、{quadratic_formula(1, 2, -8)}")
 # (3)
-print(f"(3)の答えは、{quadratic_formula(8, -6, 35)}")
+print(f"(3)の答えは、{quadratic_formula(8, -6, -35)}")
 # (4)
 print(f"(4)の答えは、{quadratic_formula(1, 0, 1)}")
