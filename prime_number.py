196150cbd94730426a474d71e9c05becdab196d0
Mahiro-3612 <m.h.s.k.australia@gmail.com>
2024-07-06 13:56:14 +0900

    ver3です笑

--- prime_number.py
+++ prime_number.py
@@ -1,4 +1,25 @@
-a = input("aの値を入力: ")
-b = input("bの値を入力: ")
+def prime_number_checker(input):
 
-# TODO
+    if input == 1:
+        return "素数ではありません"
+
+    elif input == 2:
+        return "素数です"
+
+    else:
+        possible_factor = 2
+
+        while possible_factor < input // 2 + 1:
+            if input % possible_factor == 0:
+                return "素数ではありません"
+                break
+            else:
+                possible_factor += 1
+
+    return "素数です"
+
+
+# (1)
+print(f"61は{prime_number_checker(61)}")
+# (2)
+print(f"10は{prime_number_checker(10)}")
--- trapezoidal_integral.py
+++ trapezoidal_integral.py
@@ -1,5 +1,17 @@
-from math import sin
-# --example--
-# print(sin(0))
-# >>> 0
-# -----------
+import math
+
+
+def trapezoidal_integral(func, a, b, N):
+
+    h = (b - a) / N
+
+    S = 0
+
+    for k in range(1, N + 1):
+        S += (h / 2) * (func(a + (k - 1) * h) + func(a + k * h))
+
+    return S
+
+
+# (問2)
+print(f"求める面積は{trapezoidal_integral(math.sin, 0, math.pi / 2, 100)}")
